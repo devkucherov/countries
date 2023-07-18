9ba4199cb3b595423bae02cca07ed540a03eb7f2
Pasha Kucherov <dev.kucherov@gmail.com>
2023-07-18 23:34:32 +0300

    адаптировать под мобильные утройства

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,14 +1,20 @@
 function mobileNav() {
-	// Mobile nav button
-	const navBtn = document.querySelector('.mobile-nav-btn');
-	const nav = document.querySelector('.mobile-nav');
-	const menuIcon = document.querySelector('.nav-icon');
+		// Mobile nav button
+		const navBtnOpen = document.querySelector('#modalOpen');
+		const navBtnClose = document.querySelector('#modalClose');
 
-	navBtn.onclick = function () {
-		nav.classList.toggle('mobile-nav--open');
-		menuIcon.classList.toggle('nav-icon--active');
-		document.body.classList.toggle('no-scroll');
-	};
+		const navOverlay = document.querySelector('#mobileNavOverlay');
+		const nav = document.querySelector('#mobileNav');
+
+		navBtnOpen.onclick = tooggleMobileNav;
+		navBtnClose.onclick = tooggleMobileNav;
+		navOverlay.onclick = tooggleMobileNav;
+
+		function tooggleMobileNav () {
+			navOverlay.classList.toggle('mobile-nav-overlay--open');
+			nav.classList.toggle('mobile-nav--open');
+			document.body.classList.toggle('no-scroll');
+		}
 }
 
 export default mobileNav;
