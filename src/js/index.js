4392aca7c409c7d1079cb48b46cbc47c71cb0ef7
Pasha Kucherov <dev.kucherov@gmail.com>
2023-07-18 22:03:18 +0300

    сверстать десктопную версию

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,37 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
+
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  parallax: true,
+  speed: 1000,
+
+  keyboard: {
+    enabled: true,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.slider-controls__count',
+    type: 'fraction',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '#sliderNext',
+    prevEl: '#sliderPrev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
